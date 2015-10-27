f0a0f2c86cdc5d2ed472db78c231d6797018c095
iwgang <iwgang@163.com>
2015-10-28 00:31:22 +0800

    update demo

--- app/src/main/java/cn/iwgang/countdownviewdemo/MainActivity.java
+++ app/src/main/java/cn/iwgang/countdownviewdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.iwgang.countdownviewdemo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -16,7 +17,7 @@ public class MainActivity extends ActionBarActivity implements CountdownView.OnC
 
         CountdownView mCvCountdownViewTest1 = (CountdownView)findViewById(R.id.cv_countdownViewTest1);
         mCvCountdownViewTest1.setTag("test1");
-        long time1 = (long)2 * 60 * 60 * 1000;
+        long time1 = (long)5 * 60 * 60 * 1000;
         mCvCountdownViewTest1.start(time1);
 
         CountdownView mCvCountdownViewTest2 = (CountdownView)findViewById(R.id.cv_countdownViewTest2);
@@ -32,9 +33,31 @@ public class MainActivity extends ActionBarActivity implements CountdownView.OnC
         long time4 = (long)150 * 24 * 60 * 60 * 1000;
         mCvCountdownViewTest4.start(time4);
 
-        CountdownView mCvCountdownViewTest5 = (CountdownView)findViewById(R.id.cv_countdownViewTest5);
-        long time5 = (long)2 * 60 * 60 * 1000;
-        mCvCountdownViewTest5.start(time5);
+        final CountdownView mCvCountdownViewTest5 = (CountdownView)findViewById(R.id.cv_countdownViewTest5);
+        new AsyncTask<Void, Long, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                long time = 0;
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        time += 1000;
+                        publishProgress(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            @Override
+            protected void onProgressUpdate(Long... values) {
+                super.onProgressUpdate(values);
+                mCvCountdownViewTest5.updateShow(values[0]);
+            }
+        }.execute();
+
+        CountdownView mCvCountdownViewTest6 = (CountdownView)findViewById(R.id.cv_countdownViewTest6);
+        long time6 = (long)2 * 60 * 60 * 1000;
+        mCvCountdownViewTest6.start(time6);
 
     }
 
